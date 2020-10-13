c3747374df83790e8e201680f885233b799840f7
Budi Yunior Valentino <magenzlizrd@gmail.com>
2020-10-13 14:19:26 +0700

    footer js

--- backend-emas/application/views/partials/header.php
+++ backend-emas/application/views/partials/header.php
@@ -2,34 +2,34 @@
 <html lang="en">
 
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta charset="utf-8" />
-    <title><?= $title ?></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta charset="utf-8" />
+  <title><?= $title ?></title>
 
-    <meta name="description" content="top menu &amp; navigation" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  <meta name="description" content="top menu &amp; navigation" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <!-- bootstrap & fontawesome -->
-    <link rel="shortcut icon" href="<?= site_url() ?>assets/assets/img/php.png">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/assets/css/font-awesome.css" />
+  <!-- bootstrap & fontawesome -->
+  <link rel="shortcut icon" href="<?= site_url() ?>assets/assets/img/php.png">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/bootstrap.css" />
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/assets/css/font-awesome.css" />
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- page specific plugin styles -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- page specific plugin styles -->
 
-    <!-- text fonts -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/ace-fonts.css" />
+  <!-- text fonts -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/ace-fonts.css" />
 
-    <!-- ace styles -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
+  <!-- ace styles -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/assets/css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
 
-    <!-- ace settings handler -->
-    <script src="<?php echo base_url() ?>assets/assets/js/ace-extra.js"></script>
+  <!-- ace settings handler -->
+  <script src="<?php echo base_url() ?>assets/assets/js/ace-extra.js"></script>
 
-    <!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+  <!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
 
-    <!--[if lte IE 8]>
+  <!--[if lte IE 8]>
 		<script src="../../assets/assets/js/html5shiv.js"></script>
 		<script src="../../assets/assets/js/respond.js"></script>
 		<![endif]-->
