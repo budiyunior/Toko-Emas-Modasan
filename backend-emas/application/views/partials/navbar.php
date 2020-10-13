a41bfcfdcd70b401c0411c5d0dc65bea0e48bea7
Ardiyan16 <47960612+Ardiyan16@users.noreply.github.com>
2020-10-13 09:17:23 +0700

    baru

---
+++ backend-emas/application/views/partials/navbar.php
@@ -0,0 +1,180 @@
+<div id="navbar" class="navbar navbar-default    navbar-collapse       h-navbar">
+    <script type="text/javascript">
+        try {
+            ace.settings.check('navbar', 'fixed')
+        } catch (e) {}
+    </script>
+
+    <div class="navbar-container" id="navbar-container">
+        <div class="navbar-header pull-left">
+            <!-- #section:basics/navbar.layout.brand -->
+            <a href="#" class="navbar-brand">
+                <small>
+                    <i class="fa fa-leaf"></i>
+                    LOGO
+                </small>
+            </a>
+            <button class="pull-right navbar-toggle navbar-toggle-img collapsed" type="button" data-toggle="collapse" data-target=".navbar-buttons,.navbar-menu">
+                <span class="sr-only">Toggle user menu</span>
+
+                <img src="../assets/assets/avatars/user.jpg" alt="Jason's Photo" />
+            </button>
+
+            <button class="pull-right navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#sidebar">
+                <span class="sr-only">Toggle sidebar</span>
+
+                <span class="icon-bar"></span>
+
+                <span class="icon-bar"></span>
+
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+
+        <!-- #section:basics/navbar.dropdown -->
+        <div class="navbar-buttons navbar-header pull-right  collapse navbar-collapse" role="navigation">
+            <ul class="nav ace-nav">
+                <!-- #section:basics/navbar.user_menu -->
+                <li class="light-blue user-min">
+                    <a data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <img class="nav-user-photo" src="../../assets/assets/avatars/user.jpg" alt="Jason's Photo" />
+                        <span class="user-info">
+                            <small>Welcome,</small>
+                            Jason
+                        </span>
+
+                        <i class="ace-icon fa fa-caret-down"></i>
+                    </a>
+
+                    <ul class="user-menu dropdown-menu-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+                        <li>
+                            <a href="#">
+                                <i class="ace-icon fa fa-cog"></i>
+                                Settings
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="profile.html">
+                                <i class="ace-icon fa fa-user"></i>
+                                Profile
+                            </a>
+                        </li>
+
+                        <li class="divider"></li>
+
+                        <li>
+                            <a href="#">
+                                <i class="ace-icon fa fa-power-off"></i>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- /section:basics/navbar.user_menu -->
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+<!-- /section:basics/navbar.layout -->
+<div class="main-container" id="main-container">
+    <script type="text/javascript">
+        try {
+            ace.settings.check('main-container', 'fixed')
+        } catch (e) {}
+    </script>
+
+    <!-- #section:basics/sidebar.horizontal -->
+    <div id="sidebar" class="sidebar      h-sidebar                navbar-collapse collapse">
+        <script type="text/javascript">
+            try {
+                ace.settings.check('sidebar', 'fixed')
+            } catch (e) {}
+        </script>
+
+
+        <ul class="nav nav-list">
+            <li class=" active hover">
+                <a href="index.html">
+                    <i class="menu-icon fa fa-inbox"></i>
+                    <span class="menu-text"> Barang </span>
+                </a>
+            </li>
+
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-users"></i>
+                    <span class="menu-text">
+                        Pelanggan
+                    </span>
+                </a>
+            </li>
+
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-user"></i>
+                    <span class="menu-text">
+                        Sales
+                    </span>
+                </a>
+            </li>
+
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-bar-chart"></i>
+                    <span class="menu-text">
+                        Penjualan
+                    </span>
+                </a>
+            </li>
+
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-pie-chart"></i>
+                    <span class="menu-text">
+                        Pembelian
+                    </span>
+                </a>
+            </li>
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-cart-plus"></i>
+                    <span class="menu-text">
+                        Lap. Jual
+                    </span>
+                </a>
+            </li>
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-cart-arrow-down"></i>
+                    <span class="menu-text">
+                        Lap. Beli
+                    </span>
+                </a>
+            </li>
+            <li class="hover">
+                <a href="#" class="dropdown-toggle">
+                    <i class="menu-icon fa fa-cog"></i>
+                    <span class="menu-text">
+                        Pengaturan
+                    </span>
+                </a>
+            </li>
+
+
+
+        </ul><!-- /.nav-list -->
+
+        <!-- #section:basics/sidebar.layout.minimize -->
+
+        <!-- /section:basics/sidebar.layout.minimize -->
+        <script type="text/javascript">
+            try {
+                ace.settings.check('sidebar', 'collapsed')
+            } catch (e) {}
+        </script>
+    </div>
