988d360b68342e434a5281cb45746269eaa8c982
Mochamad Yusuf Setiya Putra <72720312+yusufsetiya@users.noreply.github.com>
2020-10-14 13:25:32 +0700

    ayo

--- backend-emas/application/controllers/C_pelanggan.php
+++ backend-emas/application/controllers/C_pelanggan.php
@@ -7,9 +7,9 @@ class C_pelanggan extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('form_validation');
-        $this->load->helper('url');
-        $this->load->helper('form');
+        // $this->load->library('form_validation');
+        // $this->load->helper('url');
+        // $this->load->helper('form');
         $this->load->model('M_menu');
         $this->load->model('M_pelanggan');
     }
@@ -19,8 +19,7 @@ class C_pelanggan extends CI_Controller
     {
         $data['title'] = "Pelanggan";
         $data['menu'] = $this->M_menu->get_menu();
-        $data['pelanggan'] = $this->M_sales->get();
-        $data['jabatan'] = $this->M_sales->get_jabatan();
-        $this->load->view('admin/v_sales', $data);
+        $data['pelanggan'] = $this->M_pelanggan->get();
+        $this->load->view('admin/v_pelanggan', $data);
     }
 }
