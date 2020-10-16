f0eb82c758e6bb45e70421d7daa88ced8b0b32c1
nadhfrr <72719539+nadhfrr@users.noreply.github.com>
2020-10-16 14:48:41 +0700

    edit pelanggan
    new

--- backend-emas/application/models/M_pelanggan.php
+++ backend-emas/application/models/M_pelanggan.php
@@ -37,7 +37,21 @@ class M_pelanggan extends CI_Model
 
         $this->db->insert($this->tabel, $this);
     }
+    
+    public function update_pelanggan()
+    {
+        $post = $this->input->post();
+        $this->fc_salesid = $post['fc_kdpel'];
+        $this->fv_nama = $post['fv_nmpelanggan'];
+        $this->fc_email = $post['f_alamat'];
+        $this->fc_hp = $post['fc_telp'];
+        $this->fc_aktif = $post['fc_aktif'];
+        $this->fd_tgllahir = $post['fc_noktp'];
+        $this->fc_kdposisi = $post['f_keterangan'];
 
+        $this->db->update($this->tabel, $this, array('fc_salesid' => $post['fc_salesid']));
+    }
+    
     public function delete($id)
     {
         $this->db->where('fc_kdpel', $id);
