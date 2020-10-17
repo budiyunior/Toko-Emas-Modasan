4c4a8f17a9ed6abee3712642bd24db879f93b84b
Ardiyan16 <47960612+Ardiyan16@users.noreply.github.com>
2020-10-17 09:33:59 +0700

    hh

--- backend-emas/application/models/M_sales.php
+++ backend-emas/application/models/M_sales.php
@@ -67,15 +67,15 @@ class M_sales extends CI_Model
     public function update_sales()
     {
         $post = $this->input->post();
-        $this->fc_salesid = $post['fc_salesid'];
-        $this->fv_nama = $post['fv_nama'];
-        $this->fc_email = $post['fc_email'];
-        $this->fc_hp = $post['fc_hp'];
-        $this->fc_aktif = $post['fc_aktif'];
-        $this->fd_tgllahir = $post['fd_tgllahir'];
-        $this->fc_kdposisi = $post['fc_kdposisi'];
+        $this->fc_salesid = $post['fc_salesid_edit'];
+        $this->fv_nama = $post['fv_nama_edit'];
+        $this->fc_email = $post['fc_email_edit'];
+        $this->fc_hp = $post['fc_hp_edit'];
+        $this->fc_aktif = $post['fc_aktif_edit'];
+        $this->fd_tgllahir = $post['fd_tgllahir_edit'];
+        $this->fc_kdposisi = $post['fc_kdposisi_edit'];
 
-        $this->db->update($this->tabel, $this, array('fc_salesid' => $post['fc_salesid']));
+        $this->db->update($this->tabel, $this, array('fc_salesid' => $post['fc_salesid_edit']));
     }
 
     public function delete($id)
