dd62bfa0959c972fa761e718d017acd8d706ae26
Budi Yunior Valentino <magenzlizrd@gmail.com>
2020-10-27 13:36:45 +0700

    m

--- backend-emas/application/models/M_pengaturan.php
+++ backend-emas/application/models/M_pengaturan.php
@@ -3,4 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_pengaturan extends CI_Model
 {
+
+    private $tbadmin = "admin";
+
+    public function get_user()
+    {
+        return $this->db->get($this->tbadmin)->result();
+    }
 }
