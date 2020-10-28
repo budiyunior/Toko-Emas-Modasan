fb3103f390e3a642e1102aa04ccba804a55e07df
Budi Yunior Valentino <magenzlizrd@gmail.com>
2020-10-28 09:12:12 +0700

    Revert "s"
    This reverts commit b1edbb5e6496ba390d086dec5a7d73a3d9227d3e.

--- backend-emas/application/models/M_pengaturan.php
+++ backend-emas/application/models/M_pengaturan.php
@@ -4,15 +4,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class M_pengaturan extends CI_Model
 {
     private $tabel = "tab_akses_mainmenu";
-    private $tbadmin = "admin";
+
+
     public function save()
     {
         $post = $this->input->post();
         $this->id_menu = $post['id_menu'];
+        
     }
 
-    function get_user()
-    {
-        return $this->db->get($this->tbadmin)->result();
-    }
+
 }
